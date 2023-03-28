4d924f192c53fbb327085c2d180a382fd86aada0
arunodari <arunodari@gmail.com>
2023-03-28 21:37:08 +0545

    some changes

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-
+var items = ["Buy food", "cook food", "Eat food"];
 const port = 3000;
-
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ encoded: true }));
 app.get("/", (req, res) => {
   var today = new Date();
-  var currentDay = today.getDay();
-  if (currentDay === 6) {
-    res.write("<h1>Yay it's the weekend.</h1>");
-  } else {
-    res.sendFile(`${__dirname}/index.html`);
-  }
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+  var day = today.toLocaleDateString("en-US", options);
+  res.render("list", { kindOfDay: day, newListItem: item });
+});
+app.post("/", (req, res) => {
+  var item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
 });
 
 app.listen(port, () => {
